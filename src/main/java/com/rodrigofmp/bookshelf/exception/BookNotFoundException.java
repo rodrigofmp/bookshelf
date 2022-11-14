4c1b1f11cbc736080dae398af496e6f76142d85b
Rodrigo F. M. Petters <rodrigofmp@gmail.com>
2022-11-14 14:07:16 -0300

    Swagger docs

--- src/main/java/com/rodrigofmp/bookshelf/exception/BookNotFoundException.java
+++ src/main/java/com/rodrigofmp/bookshelf/exception/BookNotFoundException.java
@@ -2,6 +2,8 @@ package com.rodrigofmp.bookshelf.exception;
 
 public class BookNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public BookNotFoundException(long id) {
 		super("Could not find book " + id);
 	}
